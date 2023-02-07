81849de5b25fa67c9b7142a2c3a4ad56bcf99939
eknows <johnnyenos@gmail.com>
2023-02-07 15:14:00 -0500

    Create Installscript.sh

---
+++ Installscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This installs a few tools needed for training
+
+#install git
+
+sudo apt install git
+
+#install openssl
+
+sudo apt-get install openssl
+
+#install docker
+
+
+
+sudo apt install docker.io
